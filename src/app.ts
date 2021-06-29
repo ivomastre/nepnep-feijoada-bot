87324f5341613df1a952ea91e54214e6b879258e
Ivo Augusto Wanderley de Paiva <40743787+ivomastre@users.noreply.github.com>
2021-06-28 23:28:31 -0300

    chore: added heroku workaround

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import Bot from './utils/bot'
 import schedule from 'node-schedule';
 import { FACEBOOK_ACCESS_TOKEN, FACEBOOK_API_VERSION, NODE_ENV } from './config/env'
 import { connectDb } from './db';
-
+import http from 'http'
 
 connectDb()
 const facebookBot = new Bot(FACEBOOK_ACCESS_TOKEN, FACEBOOK_API_VERSION)
@@ -16,7 +16,19 @@ schedule.scheduleJob('0 * * * *', async function(){
     }
 });
 
+// workaround for free heroku
+setInterval(() => {
+  console.log("Ping!"); 
+}, 1000);
+
+// workaround for free heroku
+http.createServer(function (req, res) {
+  res.write('Pong!'); 
+  res.end();
+}).listen(process.env.PORT || '8080');
+
 if(NODE_ENV == 'dev') {
     console.log('Bot has started 🐒');
     (() => facebookBot.postToFacebook())()
 }
+
